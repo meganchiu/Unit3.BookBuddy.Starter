df0fd69cdf02853cccb0db23e8183dfc3543b5be
Megan Chiu <megan.chiu.2014@gmail.com>
2025-02-24 16:19:13 -0800

    fix format in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import { Routes, Route } from 'react-router-dom'
 import Books from './components/Books'
 import SingleBook from './components/SingleBook'
 import Register from './components/Register'
+import Login from './components/Login'
+import Navigations from './components/Navigations'
+import Account from './components/Account'
 
 function App() {
   const [token, setToken] = useState(null)
 
   return (
     <>
-    <Routes>
-      <Route path='/' element={<Books />} />
-      <Route path='/books/:id' element={<SingleBook />} />
-      <Route path='/register' element={<Register />} />
-    </Routes>
+      <Navigations />
+      <Routes>
+        <Route path='/' element={<Books />} />
+        <Route path='/books/:id' element={<SingleBook />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/account' element={<Account />} />
+      </Routes>
     </>
   )
 }

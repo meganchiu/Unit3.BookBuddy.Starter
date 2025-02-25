f0a21422db42fa9fd3aa67f0baca1205bda8fdcf
Megan Chiu <megan.chiu.2014@gmail.com>
2025-02-24 22:45:40 -0800

    add styling to make it easier to read

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,6 +3,7 @@ import SingleBook from "./SingleBook.jsx";
 export default function Books() {
   const BOOKS_API_URL = "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books"
   const [books, setBooks] = useState([]);
+  const defaultImg = "https://static.wikia.nocookie.net/gijoe/images/b/bf/Default_book_cover.jpg/revision/latest?cb=20240508080922"
 
   useEffect(() => {
     const getBooks = async () => {
@@ -19,12 +20,16 @@ export default function Books() {
 
   return (
     <>
-      <div>
+      <div className="bookContainer">
         {books.map((book) => (
           <div key={book.id} id={book.id} className="bookCard">
             <h4><b>Title: </b>{book.title}</h4>
             <h4><b>Author: </b>{book.author}</h4>
-            <img src={book.coverimage} alt={book.title} />
+            <img src={book.coverimage} alt={book.title} className="bookCardImg" 
+              onError={(e) => {
+                e.target.src = defaultImg;
+              }}/>
+            <br/>
             <SingleBook bookId={book.id} />
           </div>
         ))}

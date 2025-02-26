a1458fee732a3a3ef4dfc2b1fd4be51b5d5a2016
Megan Chiu <megan.chiu.2014@gmail.com>
2025-02-26 12:57:21 -0800

    remove unused code

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import { useAuth } from "../components/AuthContext.jsx"
 import { useNavigate } from "react-router-dom";
 

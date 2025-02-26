457a5cfe017a41093210f74c47244298c605fc8d
Megan Chiu <megan.chiu.2014@gmail.com>
2025-02-26 15:34:57 -0800

    remove comments and log statement

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -6,7 +6,7 @@ export default function Books() {
   const BOOKS_API_URL = "https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books";
   const [books, setBooks] = useState([]);
   const [filteredBooks, setFilteredBooks] = useState([]);
-  const [searchText, setSearchText] = useState(""); // Track search text
+  const [searchText, setSearchText] = useState("");
   const defaultImg = "https://static.wikia.nocookie.net/gijoe/images/b/bf/Default_book_cover.jpg/revision/latest?cb=20240508080922";
 
   useEffect(() => {
@@ -14,9 +14,8 @@ export default function Books() {
       try {
         const response = await fetch(BOOKS_API_URL);
         const data = await response.json();
-        console.log(data.books);
         setBooks(data.books);
-        setFilteredBooks(data.books); // Initially, filteredBooks should have all books
+        setFilteredBooks(data.books);
       } catch (error) {
         console.error("Error fetching all books.", error);
       }
@@ -27,7 +26,7 @@ export default function Books() {
   const handleSearch = (query) => {
     setSearchText(query);
     if (query === "") {
-      setFilteredBooks(books); // Show all books if search is empty
+      setFilteredBooks(books);
     } else {
       const filtered = books.filter((book) =>
         book.title.toLowerCase().includes(query.toLowerCase())
